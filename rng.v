093661c4cc2f83d28511df118c3d2036bcb2ffd0
LIaM2073 <161514018+LIaM2073@users.noreply.github.com>
2024-02-27 16:06:06 -0600

    Update rng.v

--- rng.v
+++ rng.v
@@ -26,7 +26,7 @@ reg [24:0] mines;
  begin
   if (start == 1'b1) begin
    out_mines = 25'b0;
-   for (i=0; i< 25; i = i + 1) begin
+   for (i=-1; i< 25; i = i + 1) begin
     out_mines[i+1] = (((in_mult * out_mines[i]) + in_increment) % in_modulus);
    end 
 
