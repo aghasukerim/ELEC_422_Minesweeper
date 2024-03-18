9eae681babbc74181913b9e3bac6792eef8c72cf
LIaM2073 <161514018+LIaM2073@users.noreply.github.com>
2024-03-18 17:16:41 -0500

    Update rng.v

--- Questa/rng.v
+++ Questa/rng.v
@@ -4,13 +4,12 @@
 //
 // Function    : Pseudo-Random Number Generator to compute placement of mines
 //-----------------------------------------------------
-module rng (in_clka, in_start, in_mult, in_increment, in_modulus, in_mines_num, out_mines);
+module rng (in_clka, in_start, in_mult, in_increment, in_mines_num, out_mines);
 //-------------Input Ports-----------------------------
 input   in_clka, in_start;
- input    in_mult;
- input    in_increment;
- input    in_modulus;
- input    in_mines_num;
+ input   [4:0] in_mult;
+ input   [4:0] in_increment;
+ input   [4:0] in_mines_num;
  //in_param: [b2,b1,b0]
  //in_mult <-- multiplier ('a')
  //in_increment <-- increment ('c')
@@ -23,21 +22,34 @@ wire    in_clka, in_start, in_mult, in_increment, in_modulus, in_mines_num;
 //-------------Output Ports Data Type------------------
 reg [24:0] mines;
 //----------Code starts Here------------------------
-/* always @ (negedge clka)
+ //--------Internal Variables-----------------------
+
+ //-------------------------------------------------
+ /*always @ (negedge clka)
  begin
   if (start == 1'b1) begin
    out_mines = 25'b0;
-   for (i=-1; i< in_mines_num; i = i + 1) begin
+   for (i=-1; i< in_mines_num; i = (i + 1)) begin
     if (i == -1) begin
      out_mines[0] = (((in_mult) + in_increment) % in_modulus);
     end else begin
-     out_mines[i+1] = (((in_mult * out_mines[i]) + in_increment) % in_modulus);
+     out_mines[i] = (((in_mult * out_mines[i]) + in_increment) % in_modulus);
     end
     //Rearranges the out_mines to ensure
     for (i=0; i<in_mines_num; i= i + 1) begin
-     out_mines[i] = out_mines[i + ({$random}%25)]
+     out_mines[i] = out_mines[i + ({$random} % 25)]
     end
    end 
   end
  end 
-*/endmodule
+ */
+ always @ (negedge clka) begin
+  if (start == 1'b1) begin
+   out_mines = 25'b0;
+   for (i = 0; i < in_mines_num; ) begin
+    out_mines
+   end
+  end
+ end
+   
+endmodule
