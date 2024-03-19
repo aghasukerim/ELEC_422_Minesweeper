061c141459eba563818790d2068bedb2368a1467
LIaM2073 <161514018+LIaM2073@users.noreply.github.com>
2024-03-19 16:21:43 -0500

    Updated top_module.v

--- Questa/top_module.v
+++ Questa/top_module.v
@@ -5,13 +5,17 @@
 // Function    : top file call main_FSM controller.
 //-----------------------------------------------------
 module top_module (in_clka, in_clkb, in_restart, in_mult, in_increment, in_mines_num, out_state_main, in_place, in_data_in, in_data,
- out_start, out_place_done, out_mines, out_load, out_temp_data_in, out_decode, out_decode_done,
+ out_start, out_place_done, out_rng_done, out_mines, out_load, out_temp_data_in, out_decode, out_decode_done,
  out_alu, out_alu_done, out_gameover, out_win, out_global_score, out_n_nearby, out_temp_decoded, out_temp_cleared,
  out_display, out_display_done);
  
 //-------------Input Ports-----------------------------
-input   in_clka, in_clkb, in_restart, in_place, in_data_in, in_increment, in_mult, in_mines_num;
+input   in_clka, in_clkb, in_restart;
 input   [4:0] in_data;
+input  in_start, in_place, data_in;
+input  [4:0] in_mult;
+input  [4:0] in_increment;
+input  [4:0] in_mines_num;
 //-------------Output Ports----------------------------
 output  [3:0] out_state_main; 
 output  out_start, out_place_done, out_mines, out_load, out_temp_data_in, out_decode, out_decode_done;
@@ -21,11 +25,17 @@ output [31:0] out_global_score;
 output [24:0] out_temp_decoded;
 output [24:0] out_temp_cleared;
 output [1:0] out_n_nearby;
+output [2:0] rng_done;
+output out_place_done;
+output [24:0] out_mines;
 //-------------Input ports Data Type-------------------
-wire    in_clka, in_clkb, in_restart; 
-integer in_increment, in_mult, in_modulus, in_mines_num;
-wire    in_place, in_data_in;
-wire    [4:0] in_data;
+wire    in_clka, in_clkb, in_restart, in_start in_place, in_place_done; 
+reg [2:0] in_mult;
+reg [4:0] in_increment;
+reg [4:0] in_modulus;
+reg [4:0] in_mines_num
+wire in_place, in_data_in;
+wire [4:0] in_data;
 //-------------Output Ports Data Type------------------
 wire    [3:0] out_state_main;
 wire    out_start;
@@ -96,6 +106,10 @@ dp dp_ALU  (.clka (in_clka),
   .in_increment(in_increment),
   .in_modulus(in_modulus),
   .out_mines(out_mines),
-  .in_mines_num(in_mines_num)
+  .in_mines_num(in_mines_num),
+  .in_place(in_place),
+  .out_place_done(out_place_done),
+  .data_in(in_data_in),
+  .rng_done(out_rng_done)
   ); 
 endmodule // End of Module top_module
