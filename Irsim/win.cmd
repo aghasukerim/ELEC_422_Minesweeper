4fb9e32b1cb134b588ea237c7807447bfec3e374
Alexis Le Glaunec <alexisdu51151@gmail.com>
2024-04-17 20:07:39 -0500

    win.cmd updated with R

--- Irsim/win.cmd
+++ Irsim/win.cmd
@@ -37,7 +37,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 1
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -48,7 +48,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d1 0d1 0d1 0d1 0d1 0d1 0d1 0d1 0d1
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 2
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -59,7 +59,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d2 0d2 0d2 0d2 0d2 0d2 0d2 0d2 0d2
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 3
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -70,7 +70,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 4
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -81,7 +81,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d4 0d4 0d4 0d4 0d4 0d4 0d4 0d4 0d4
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 5
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -92,7 +92,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d5 0d5 0d5 0d5 0d5 0d5 0d5 0d5 0d5
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 7
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -103,7 +103,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 8
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -114,7 +114,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d8 0d8 0d8 0d8 0d8 0d8 0d8 0d8 0d8
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 9
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -125,7 +125,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d9 0d9 0d9 0d9 0d9 0d9 0d9 0d9 0d9
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 10
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -136,7 +136,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d10 0d10 0d10 0d10 0d10 0d10 0d10 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 11
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -147,7 +147,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d11 0d11 0d11 0d11 0d11 0d11 0d11 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 12
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -158,7 +158,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d12 0d12 0d12 0d12 0d12 0d12 0d12 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 13
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -169,7 +169,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d13 0d13 0d13 0d13 0d13 0d13 0d13 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 14
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -180,7 +180,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d14 0d14 0d14 0d14 0d14 0d14 0d14 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 15
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -191,7 +191,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d15 0d15 0d15 0d15 0d15 0d15 0d15 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 16
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -202,7 +202,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d16 0d16 0d16 0d16 0d16 0d16 0d16 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 18
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -213,7 +213,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d18 0d18 0d18 0d18 0d18 0d18 0d18 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 19
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -224,7 +224,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d19 0d19 0d19 0d19 0d19 0d19 0d19 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 20
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -235,7 +235,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d20 0d20 0d20 0d20 0d20 0d20 0d20 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 21
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -246,7 +246,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d21 0d21 0d21 0d21 0d21 0d21 0d21 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 22
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -257,7 +257,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d22 0d22 0d22 0d22 0d22 0d22 0d22 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Clear cell 24
 
 |	cycle        	1 2 x 3 4 5 6 7 8 9 0 1 2 3 4 5 6 
@@ -268,7 +268,7 @@ V	in_data		0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d0 0d24 0d24 0d24 0d24 0d24 0d24 0d24 0d
 V	in_mult		0d0 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 0d7 
 V	in_incr		0d0 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 0d6 
 V	in_n_mines	0d0 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3 0d3  
-
+R
 | Two phase clock with non-overlap period - same as Questa testbench
 clock in_clka 0 1 0 0
 clock in_clkb 0 0 0 1
